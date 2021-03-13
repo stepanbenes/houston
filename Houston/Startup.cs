fef658ab9161afa34b6b21498689c37b74505fa9
Štěpán Beneš <benes.stepan@gmail.com>
2021-03-13 13:47:52 +0100

    Update Startup

--- Houston/Startup.cs
+++ Houston/Startup.cs
@@ -17,18 +17,15 @@ namespace Houston
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddWebSocketManager();
+            services.AddMvc();
+            services.AddWebSocketManager(); // registers also NotificationsMessageHandler
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
+            app.UseStaticFiles();
+            app.UseWebSockets();
 
             app.UseEndpoints(endpoints =>
             {
