550d0ba084be17ce45373d48a3c8eaaed832e558
Štěpán Beneš <benes.stepan@gmail.com>
2021-03-13 13:37:08 +0100

    Add project files.

---
+++ Houston/NotificationsMessageHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebSocketManager;
+using WebSocketManager.Common;
+
+namespace Houston
+{
+    public class NotificationsMessageHandler : WebSocketHandler
+    {
+        public NotificationsMessageHandler(WebSocketConnectionManager webSocketConnectionManager)
+            : base(webSocketConnectionManager, new StringMethodInvocationStrategy())
+        {
+            ((StringMethodInvocationStrategy)MethodInvocationStrategy).On("test", args => {
+				Console.WriteLine(args);
+            });
+        }
+    }
+}
